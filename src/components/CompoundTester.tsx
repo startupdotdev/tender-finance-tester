9efba417499d3fde53bdf12284ecfc71051cce27
Ryan LaBouve <imryanlabouve@gmail.com>
2022-03-08 16:17:32 -0600

    Example in ethers

--- src/components/CompoundTester.tsx
+++ src/components/CompoundTester.tsx
@@ -5,7 +5,8 @@ import Compound from '@compound-finance/compound-js';
 import { Web3ReactContextInterface } from '@web3-react/core/dist/types';
 import { CompoundInstance } from '@compound-finance/compound-js/dist/nodejs/types';
 import CompoundFunctionFactory  from "./compound/compoundFunctionFactory";
-import { BigNumber } from 'ethers';
+import { BigNumber, ethers } from 'ethers';
+import sampleAbi from '../lib/connectors/sampleAbi';
 
 const CompoundTester = () => {
 
@@ -13,15 +14,22 @@ const CompoundTester = () => {
 
   // @ts-ignore
   const compound: CompoundInstance = new Compound(web3React.library?.getSigner());
+  const testaddr: string = "0xd6801a1DfFCd0a410336Ef88DeF4320D6DF1883e";
 
   console.log("compound", compound);
 
   // @ts-ignore
-  function supply(token: string, value: number | string | BigNumber) {
+  async function supply(token: string, value: string) {
     // TODO validate token
     // TODO: validate number
     // @ts-ignore
-    compound.supply(token, value);
+    // compound.supply(token, value);
+
+    // TODO: start with Rinkeby cEth 0xd6801a1DfFCd0a410336Ef88DeF4320D6DF1883e
+
+    let contract = new ethers.Contract(testaddr, sampleAbi, web3React.library?.getSigner());
+    let tx = await contract.mint({value: ethers.utils.parseUnits(value, 6) });
+    // TODO: test this
   }
 
   function borrow(token: string, value: number | string | BigNumber) {
@@ -41,7 +49,9 @@ const CompoundTester = () => {
   return (
     <div className="wallet-info">
       <h2>Compound SDK</h2>
+      (supply using ethers directly)<br/>
       <CompoundFunctionFactory name="Supply" fn={supply} />
+      (borrow and redeem using SDK)<br/>
       <CompoundFunctionFactory name="Borrow" fn={borrow} />
       <CompoundFunctionFactory name="Redeem" fn={redeem} />
     </div>
